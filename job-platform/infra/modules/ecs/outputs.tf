e5a1b9ae4a7bac638b258221e629391b08d736c8
Ygor <ygor@email.com>
2026-02-25 22:00:25 -0300

    feat(infra): enable s3 backend and export ecs outputs

--- job-platform/infra/modules/ecs/outputs.tf
+++ job-platform/infra/modules/ecs/outputs.tf
@@ -13,3 +13,15 @@ output "api_service_name" {
 output "worker_service_name" {
   value = aws_ecs_service.worker.name
 }
+
+output "tasks_security_group_id" {
+  value = aws_security_group.tasks.id
+}
+
+output "api_task_definition_arn" {
+  value = aws_ecs_task_definition.api.arn
+}
+
+output "worker_task_definition_arn" {
+  value = aws_ecs_task_definition.worker.arn
+}
